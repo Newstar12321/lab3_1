e299c6821eb6eacf1895012700be5523398f7665
Евтений <@DESKTOP-K29GN5E>
2023-10-08 19:27:44 +0700

    Добавьте файлы проекта.

---
+++ lab3_1/lab3_1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num;
+	bool a = true;
+	while (a == true)
+	{
+		setlocale(LC_ALL, "ru");
+		cout << "Введите положительное трехзначное число ";
+		cin >> num;
+		if ((num > 0)&&(num/100<10)&&(num>=100))
+		{
+			a = false;
+		}
+		else
+		{
+			a = true;
+		}
+	}
+	if (
+		((num / 100) != (num % 100 / 10)) && 
+		((num / 100) != (num % 10)) && 
+		((num % 100 / 10) != (num % 10))
+		)
+	{
+		cout << "Все цифры числа различны";
+	}
+	else
+	{
+		cout << "Не все цифры числа различны";
+	}
+}
